34121a54af881da5c6d6a886bc2c7743a8334eaf
Matthew <matthewjcrosby1@gmail.com>
2023-03-28 14:01:46 +0700

    cleanup

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,4 +1,6 @@
-import PrismaClient from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
 
 const categories = [
   'electronics',
@@ -9,7 +11,15 @@ const categories = [
 
 async function main() {
   console.log('Start seeding ...');
-  categories.forEach((cat) => {
+  categories.forEach(async (cat) => {
+    await prisma.category.create({
+      data: {
+        name: cat,
+      },
+
+    });
+    console.log(`Added category ${cat}`);
   });
+
   console.log('Seeding finished.');
 }
